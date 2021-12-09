3deb304b5a9c22a0f427e319acabaab9d0136836
Mateus Dias <mateuussilvapb@gmail.com>
2021-12-09 09:46:23 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/mateuussilvapb/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.mateuussilvapb.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mateuussilvapb.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+	
+}
